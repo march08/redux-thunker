94f95486e95e54790f8405e443838405592b41c0
Roman Nguyen <march08@outlook.com>
2018-03-14 23:51:00 +0100

    add test

---
+++ src/__test__/thunk.test.js
@@ -0,0 +1,112 @@
+import { enhanceArguments, thunk } from '../index';
+
+/**
+ * mock functions
+ */
+const getStateMock = () => ({
+  user: {
+    name: 'Emma Watson',
+    occupation: 'Actress',
+    status: 'Beautiful',
+  },
+});
+
+const dispatchMock = () => {};
+
+const defaultArguments = {
+  getState: getStateMock,
+};
+
+describe('Thunker Middleware', () => {
+  const nextHandler = thunk({
+    dispatch: dispatchMock,
+    getState: getStateMock,
+  });
+
+  test('Must return a function to handle action', () => {
+    const actionHandler = nextHandler();
+    expect(typeof actionHandler).toBe('function');
+  });
+
+  test('run action with dispatch and getState', (done) => {
+    const actionHandler = nextHandler();
+
+    actionHandler(({ dispatch, getState }) => {
+      expect(dispatch).toEqual(dispatchMock);
+      expect(getState).toEqual(getStateMock);
+      done();
+    });
+  });
+
+  test('pass to next if not action', (done) => {
+    const actionTest = {
+      type: 'action',
+    };
+    const actionHandler = nextHandler((action) => {
+      expect(action).toEqual(actionTest);
+      done();
+    });
+
+    actionHandler(actionTest);
+  });
+
+  const nextHandlerCompatible = thunk.extraArguments({
+    config: {
+      reduxThunkCompatible: true,
+    },
+  })({
+    dispatch: dispatchMock,
+    getState: getStateMock,
+  });
+
+  describe('Compatible thunk', () => {
+    test('run action with dispatch and getState', (done) => {
+      const actionHandler = nextHandlerCompatible();
+
+      actionHandler((dispatch, getState) => {
+        expect(dispatch).toEqual(dispatchMock);
+        expect(getState).toEqual(getStateMock);
+        done();
+      });
+    });
+  });
+});
+
+describe('Enhancing Arguments', () => {
+  test('No arguments to be enhanced, should return null', () => {
+    // first param for enhanceArguments
+    const suit = [null, {}, undefined];
+
+    suit.forEach((arg) => {
+      const enhancedArgs = enhanceArguments(arg, defaultArguments);
+      expect(enhancedArgs).toBeNull();
+    });
+  });
+
+  test('To be enhanced is not a function, error', () => {
+    // first param for enhanceArguments
+    const suit = [
+      {
+        notAFunction: {
+          sadly: 'an object',
+        },
+      },
+    ];
+
+    suit.forEach((arg) => {
+      expect(() => enhanceArguments(arg, defaultArguments)).toThrowError();
+    });
+  });
+  test('Arguments to be enhanced', () => {
+    // first param for enhanceArguments
+    const input = {
+      f: ({ getState }) => () => ({
+        type: 'test',
+        payload: getState(),
+      }),
+    };
+    const enhanced = enhanceArguments(input, defaultArguments);
+    expect(enhanced).toHaveProperty('f');
+    expect(typeof enhanced.f).toBe('function');
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
-const defaultConfig = {
+export const defaultConfig = {
   reduxThunkCompatible: false,
 };
 
-const enhanceArguments = (extraArguments, injectedArguments) => {
-  if (!extraArguments) {
+export const enhanceArguments = (extraArguments, injectedArguments) => {
+  if (!extraArguments || Object.keys(extraArguments).length === 0) {
     return null;
   }
   const keys = Object.keys(extraArguments);
-  const enhancedArguments = keys.reduce(
-    (ea, key) => ({
+  const enhancedArguments = keys.reduce((ea, key) => {
+    const toBeEnhanced = extraArguments[key];
+    if (typeof toBeEnhanced !== 'function') {
+      // eslint-disable-next-line max-len
+      throw new Error(`[redux-thunker] Property ${key} is not a function. Only functions can be enhanced.`);
+    }
+    return {
       ...ea,
-      key: extraArguments[key](injectedArguments),
-    }),
-    {},
-  );
+      [key]: toBeEnhanced(injectedArguments),
+    };
+  }, {});
   return enhancedArguments;
 };
 
-const thunker = ({
+const thunkerMiddleware = ({
   extraArguments,
   extraArgumentsEnhanced,
   config: { reduxThunkCompatible } = defaultConfig,
-}) => ({ dispatch, getState }) => next => (action) => {
+} = {}) => ({ dispatch, getState }) => next => (action) => {
   if (typeof action === 'function') {
-    // enhance desired arguments
+    /**
+     * enhance desired arguments
+     */
     const enhancedArguments = enhanceArguments(extraArgumentsEnhanced, {
       dispatch,
       getState,
@@ -33,30 +39,32 @@ const thunker = ({
      * if backward compatibility with redux-thunk is required
      *
      * usage: (dispatch, getState, { ...extraArgumentsAndEnhancedArguments }) =>
-     */
-    if (reduxThunkCompatible) {
-      return next(action(dispatch, getState, {
-        ...extraArguments,
-        ...enhancedArguments,
-      }));
-    }
-
-    /**
-     * otherwise
+     *
+     * otherwise by default
      *
      * usage: ({ dispatch, getState, ...extraArgumentsAndEnhancedArguments }) =>
      *
      * so you don't have to pick disptach or getState when you don't need it
      */
-    next(action({
-      dispatch,
-      getState,
-      ...extraArguments,
-      ...enhancedArguments,
-    }));
+    return reduxThunkCompatible
+      ? action(dispatch, getState, {
+        ...extraArguments,
+        ...enhancedArguments,
+      })
+      : action({
+        dispatch,
+        getState,
+        ...extraArguments,
+        ...enhancedArguments,
+      });
   }
 
   return next(action);
 };
 
-export default thunker;
+const thunk = thunkerMiddleware();
+thunk.extraArguments = thunkerMiddleware;
+
+export { thunk };
+
+export default thunkerMiddleware;
